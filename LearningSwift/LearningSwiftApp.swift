60536ba1c10b94fad3e594b4e338cafbb2fa399d
Serezha <evan32@yandex.ru>
2023-03-30 01:14:30 +0600

    Initial Commit

---
+++ LearningSwift/LearningSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningSwiftApp.swift
+//  LearningSwift
+//
+//  Created by Serezha on 30.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct LearningSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
