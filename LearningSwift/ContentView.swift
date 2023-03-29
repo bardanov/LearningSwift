f82bdfe040892f6defbbd713ebbc0e3389dffa84
rediskapupsik <116041533+rediskapupsik@users.noreply.github.com>
2023-03-30 01:39:01 +0600

    Update ContentView.swift

--- LearningSwift/ContentView.swift
+++ LearningSwift/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, Pupsik Wold")
+            Text("Hheeeeeeeyyy there")
         }
         .padding()
     }
