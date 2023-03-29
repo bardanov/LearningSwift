893314a4683e21d35b539af99934ff74aead0e9c
rediskapupsik <116041533+rediskapupsik@users.noreply.github.com>
2023-03-30 01:39:43 +0600

    Change greeting message

--- LearningSwift/ContentView.swift
+++ LearningSwift/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hheeeeeeeyyy there")
+            Text("Hheeeeeyyy there")
         }
         .padding()
     }
